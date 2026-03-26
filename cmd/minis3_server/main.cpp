5fbd182857b45061a8da70e6e35208e8fa00d933
ZhangPengWei <980155872@qq.com>
2026-03-26 21:20:33 +0800

    同步当前未提交改动

--- cmd/minis3_server/main.cpp
+++ cmd/minis3_server/main.cpp
@@ -1,3 +1,11 @@
+/*
+ * @Author: ZhangPengWei 980155872@qq.com
+ * @Date: 2026-03-26 20:45:44
+ * @LastEditors: ZhangPengWei 980155872@qq.com
+ * @LastEditTime: 2026-03-26 20:50:17
+ * @FilePath: \ZLS3\cmd\minis3_server\main.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 #include "server/server.h"
 #include "util/config.h"
 #include <iostream>
